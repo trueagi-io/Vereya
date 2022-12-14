96edaf14d19fe54dfac5ee8bb4b616c477ab77c4
Anatoly Belikov <abelikov@singularitynet.io>
2022-12-14 16:58:00 +0400

    fix world creation

--- src/main/java/io/singularitynet/MissionHandlers/WorldUtil.java
+++ src/main/java/io/singularitynet/MissionHandlers/WorldUtil.java
@@ -6,6 +6,7 @@ import net.minecraft.client.world.GeneratorOptionsHolder;
 import net.minecraft.entity.passive.PandaEntity;
 import net.minecraft.resource.DataPackSettings;
 import net.minecraft.server.SaveLoader;
+import net.minecraft.util.FileNameUtil;
 import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryEntry;
@@ -18,6 +19,7 @@ import net.minecraft.world.dimension.DimensionOptions;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.dimension.DimensionTypes;
 import net.minecraft.world.gen.GeneratorOptions;
+import net.minecraft.world.gen.WorldPreset;
 import net.minecraft.world.gen.WorldPresets;
 import net.minecraft.world.level.LevelInfo;
 import net.minecraft.world.level.storage.LevelStorage;
@@ -33,31 +35,16 @@ public class WorldUtil {
         UUID uuid = UUID.randomUUID();
         String worldName = uuid.toString().substring(0, 5);
         String levelName = "Vereya-test" + worldName;
-        LevelInfo levelInfo;
         GameRules gameRules = new GameRules();
-        GeneratorOptions generatorOptions = WorldUtil.getGeneratorOptionsDefault(hardcore, OptionalLong.of(seed), levelName);
 
-        levelInfo = new LevelInfo(levelName.trim(), GameMode.DEFAULT,
-                hardcore, difficulty,
-                true, gameRules, DataPackSettings.SAFE_MODE);
         MinecraftClient client = MinecraftClient.getInstance();
         DynamicRegistryManager.Immutable dynamicRegistryManager = DynamicRegistryManager.createAndLoad().toImmutable();
+        GeneratorOptions generatorOptions = WorldPresets.createDefaultOptions(dynamicRegistryManager, seed);
+        LevelInfo levelInfo = new LevelInfo(levelName.trim(),
+                GameMode.DEFAULT, hardcore, difficulty, true,
+                gameRules,
+                DataPackSettings.SAFE_MODE);
         client.createIntegratedServerLoader().createAndStart(levelName, levelInfo, dynamicRegistryManager, generatorOptions);
-    }
-
-
-    static GeneratorOptions getGeneratorOptionsDefault(boolean hardcore, OptionalLong seed, String levelName) throws Exception {
-        DynamicRegistryManager.Immutable immutable = DynamicRegistryManager.createAndLoad().toImmutable();
-        GeneratorOptions generatorOptions = WorldPresets.createDefaultOptions(immutable, seed.getAsLong());
-        return generatorOptions.withHardcore(hardcore, seed);
-    }
-
-    static GeneratorOptions getGeneratorOptionsDefault1(boolean hardcore, OptionalLong seed){
-        SimpleRegistry<DimensionOptions> mutableRegistry = new SimpleRegistry<DimensionOptions>(Registry.DIMENSION_KEY,
-                Lifecycle.experimental(), null);
-        GeneratorOptions generatorOptions = new GeneratorOptions(seed.getAsLong(), true, false,
-                mutableRegistry);
-        return generatorOptions.withHardcore(hardcore, seed);
     }
 
     public static void createLevelFlat(boolean hardcore,

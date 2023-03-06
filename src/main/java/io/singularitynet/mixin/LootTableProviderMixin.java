ef3a7304d7177d72e01a1c8b4e794fbd771582b0
Innokenty <innokenty@singularitynet.io>
2023-03-06 11:32:59 +0300

    loot table parser added

--- src/main/java/io/singularitynet/mixin/LootTableProviderMixin.java
+++ src/main/java/io/singularitynet/mixin/LootTableProviderMixin.java
@@ -1,2 +1,18 @@
-package io.singularitynet.mixin;public class LootTableProviderMixin {
+package io.singularitynet.mixin;
+
+import net.minecraft.data.server.loottable.LootTableProvider;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+import java.util.Set;
+
+@Mixin(LootTableProvider.class)
+public interface LootTableProviderMixin {
+    @Accessor("lootTableIds")
+    Set<Identifier> getlootTableIds();
+
+    @Accessor("lootTypeGenerators")
+    List<LootTableProvider.LootTypeGenerator> getlootTypeGenerators();
 }

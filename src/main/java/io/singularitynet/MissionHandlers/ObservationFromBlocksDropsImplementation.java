7950bc434ec32f20aa7bee5753e508ff29357a41
Anatoly Belikov <awbelikov@gmail.com>
2024-06-28 11:43:37 +0300

    Update src/main/java/io/singularitynet/MissionHandlers/ObservationFromBlocksDropsImplementation.java

--- src/main/java/io/singularitynet/MissionHandlers/ObservationFromBlocksDropsImplementation.java
+++ src/main/java/io/singularitynet/MissionHandlers/ObservationFromBlocksDropsImplementation.java
@@ -17,7 +17,6 @@ import net.minecraft.util.math.random.RandomSeed;
 import net.minecraft.util.math.random.RandomSequence;
 import org.jetbrains.annotations.Nullable;
 //import net.minecraft.loot.LootDataKey;
-//import net.minecraft.loot.LootDataLookup;
 import net.minecraft.loot.LootDataType;
 import net.minecraft.loot.LootTable;
 import org.apache.logging.log4j.LogManager;

5160649c08a380f191174987e34769c56614725e
Roman Kozin <enatik14@gmail.com>
2024-12-23 12:24:38 +0300

    changed back to tp (it's faster)

--- src/main/java/io/singularitynet/MissionHandlers/CommandForDiscreteRobotNavigationImplementation.java
+++ src/main/java/io/singularitynet/MissionHandlers/CommandForDiscreteRobotNavigationImplementation.java
@@ -2,23 +2,24 @@ package io.singularitynet.MissionHandlers;
 
 import io.singularitynet.projectmalmo.MissionInit;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.util.math.Vec3d;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
 
 public class CommandForDiscreteRobotNavigationImplementation extends CommandBase {
 
-    private boolean moveAgent(Vec3d movement){
+    private static final Logger LOGGER = LogManager.getLogger(CommandForDiscreteRobotNavigationImplementation.class.getName());
+
+    private boolean moveAgent(String movement){
         try {
-            ClientPlayerEntity player = MinecraftClient.getInstance().player;
-            Vec3d newPos = player.getPos().add(movement);
-            player.setPosition(newPos);
+            MinecraftClient client = MinecraftClient.getInstance();
+            LOGGER.debug("moving agent to " + movement);
+            client.player.networkHandler.sendCommand("tp " + movement);
             return true;
         }  catch (Exception e) {
-            LogManager.getLogger().warn("Failed to increment player's pos to " + movement);
+            LogManager.getLogger().warn("Failed to move agent to " + movement);
             LogManager.getLogger().warn(e);
             return false;
         }
@@ -32,11 +33,11 @@ public class CommandForDiscreteRobotNavigationImplementation extends CommandBase
         {
             return false;
         }
-        Map<String, Vec3d> moveMap = Map.of(
-                "movewest", new Vec3d(-1, 0, 0),
-                "moveeast", new Vec3d(1, 0, 0),
-                "movenorth", new Vec3d(0, 0, -1),
-                "movesouth", new Vec3d(0, 0, 1)
+        Map<String, String> moveMap = Map.of(
+                "movewest", "~-1 ~ ~",
+                "moveeast", "~1 ~ ~",
+                "movenorth", "~ ~ ~-1",
+                "movesouth", "~ ~ ~1"
         );
         String parameters[] = parameter.split(" ");
         if (parameters.length != 1) return false;

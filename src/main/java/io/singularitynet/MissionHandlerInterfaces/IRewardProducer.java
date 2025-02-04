4f3291fa9cf60c42b34bbd44898b07276a0d0807
Roman Kozin <enatik14@gmail.com>
2025-02-04 12:58:32 +0300

    producing reward for specific trigger (by the name of the class)

--- src/main/java/io/singularitynet/MissionHandlerInterfaces/IRewardProducer.java
+++ src/main/java/io/singularitynet/MissionHandlerInterfaces/IRewardProducer.java
@@ -1,8 +1,11 @@
 package io.singularitynet.MissionHandlerInterfaces;
 
+import io.singularitynet.MissionHandlers.MultidimensionalReward;
 import io.singularitynet.projectmalmo.MissionInit;
 
 public interface IRewardProducer {
     public void cleanup();
     public void prepare(MissionInit missionInit);
+    public void getReward(MultidimensionalReward reward);
+    public void produceReward(Class<? extends IRewardProducer> clazz);
 }
